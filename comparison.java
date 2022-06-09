0b04485d2648344d9086b63cc6cb5c24ca4930a6
Priyanka-Marandi <priyankamarandi0303@gmail.com>
2022-06-09 19:07:51 +0530

    init

---
+++ comparison.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class comparison
+{
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in)
+        int num1;
+        int num2;
+        System.out.print("Enter the first integer:");
+        num1 = in.nextInt();
+        System.out.print("Enter the second integer:");
+        num2 = in.nextInt();
+        if(num1 == num2)
+        System.out.printf("%d == %d %n",num1,num2);
+        if(num1 != num2)
+        System.out.printf("%d != %d %n",num1,num2);
+        if(num1 < num2)
+        System.out.printf("%d < %d %n",num1,num2);
+        if(num1 > num2)
+        System.out.printf("%d > %d %n",num1,num2);
+        if(num1 <= num2)
+        System.out.printf("%d <= %d %n",num1,num2);
+        if(num1 >= num2)
+        System.out.printf("%d >= %d %n",num1,num2);
+    }
+}
