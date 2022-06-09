2f0d9f23fed8e6aad7fa88c3ad794db26d80c14a
Priyanka-Marandi <priyankamarandi0303@gmail.com>
2022-06-09 19:02:57 +0530

    init

--- addition.java
+++ addition.java
@@ -3,12 +3,13 @@ public class addition
 {
     public static void main(String args[])
     {
-        Scanner in = new Scanner(System.in);
+        
         int num1,num2,sum;
+        Scanner input = new Scanner(System.in);
         System.out.print("Enter the first number");
-        num1 = in.nnextInt();
+        num1 = input.nextInt();
         System.out.print("Enter the second number");
-        num2 = in.nextInt();
+        num2 = input.nextInt();
         sum = num1 + num2;
         System.out.printf("Sum is %d%n",sum);
     }
